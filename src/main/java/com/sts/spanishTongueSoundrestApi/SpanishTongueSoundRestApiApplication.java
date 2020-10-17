a04cf37b6f9ed0896005d1792b483844f2a3f57d
jperezviloria <protobot.dev@gmail.com>
2020-10-17 16:01:32 -0300

    creating project

---
+++ src/main/java/com/sts/spanishTongueSoundrestApi/SpanishTongueSoundRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sts.spanishTongueSoundrestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpanishTongueSoundRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpanishTongueSoundRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sts/spanishTongueSoundrestApi/SpanishTongueSoundRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sts.spanishTongueSoundrestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpanishTongueSoundRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
