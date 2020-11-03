903c063237101f2e4cc0a3831c565be85c0f410e
ancoder-ba <andrese.959595@gmail.com>
2020-11-03 10:50:06 -0300

    SmtpSender fix

--- src/main/java/com/sts/spanishTongueSoundrestApi/service/implementation/SmtpSenderImplementeation.java
+++ src/main/java/com/sts/spanishTongueSoundrestApi/service/implementation/SmtpSenderImplementeation.java
@@ -11,7 +11,7 @@ import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
 
 @Service
-public class SmtpMailSenderImplementation {
+public class SmtpSenderImplementeation {
 
     @Autowired
     private JavaMailSender javaMailSender;
